4b7df9e4d929030540a03885cbce65b622d46da3
moon <moonjh9392@gmail.com>
2023-02-24 18:03:23 +0900

    hide api key

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = 'bb0b830f3457801698cbd268e85c8b9f';
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/old-blog/:path', //변경될 path로 보낼 url
+        destination: '/new-blog/:path', //source로 오면 destination으로 보냄
+        permanent: false, //브라우저나 검색엔진이 이 정보를 기억하는지 여부
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies', //유저가 보는 url
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`, //실제로 보내는 url
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
